b978398c0a0155cf74100c26ef7a53f3969bf02e
dinhnguyencntt <dn1994099@gmail.com>
2024-02-19 16:44:32 +0700

    Add project files.

---
+++ giaiptbac1 moi.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b;
+	float x;
+	cout << "ung dung giai phuong trinh bac I\n";
+	cout << "nhap a:"; cin >> a;
+	cout << "nhap b:"; cin >> b;
+	if (a == 0)
+	{
+		if (b != 0)
+		{
+			cout << "pt vô nghiem\n";
+		}
+		else {
+			cout << "pt dung voi moi x\n";
+		}
+	}
+	else {
+		x = (float)-b / a;
+		cout << "ket qua x=" << x << endl;
+	}
+	return 0;
+ }
